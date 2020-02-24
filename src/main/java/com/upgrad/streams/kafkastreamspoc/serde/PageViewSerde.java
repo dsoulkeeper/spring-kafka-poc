c684fa731b1ea66cbf01bb5b0c94511487af196d
Yogesh Badke <yogesh.badke@upgrad.com>
2020-02-24 15:08:34 +0530

    Removed unwanted code & properties

--- src/main/java/com/upgrad/streams/kafkastreamspoc/serde/PageViewSerde.java
+++
@@ -1,7 +0,0 @@
-package com.upgrad.streams.kafkastreamspoc.serde;
-
-import com.upgrad.streams.kafkastreamspoc.model.PageView;
-import org.springframework.kafka.support.serializer.JsonSerde;
-
-public class PageViewSerde extends JsonSerde<PageView> {
-}
